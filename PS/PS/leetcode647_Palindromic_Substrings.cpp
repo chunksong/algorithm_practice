5342ab507f405b9596dc41c15401c07fa5cbdde5
HES SONG <chunksong23@gmail.com>
2017-09-14 00:54:59 -0700

    solve DP
    need to study more about  palindrom

--- PS/PS/leetcode647_Palindromic_Substrings.cpp
+++ PS/PS/leetcode647_Palindromic_Substrings.cpp
@@ -10,6 +10,9 @@
  Given a string, your task is to count how many palindromic substrings in this string.
  
  The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+ 
+ 
+ NEED TO STUDY MORE ABOUT IT
  */
 
 class Solution {
